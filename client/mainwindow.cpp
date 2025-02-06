0a73c0fa8dc17df3f0ae31ad7d69667441720dda
gravoo <gravoo@yahoo.com>
2025-02-06 12:52:37 +0100

    change layout to tabView

--- client/mainwindow.cpp
+++ client/mainwindow.cpp
@@ -1,6 +1,6 @@
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
 #include <QDebug>
+#include <QLabel>
 #include <QMessageBox>
 #include <QtWidgets/QTextBrowser>
 #include <memory>
@@ -9,18 +9,71 @@
 
 MainWindow::MainWindow(QWidget* parent)
   : QMainWindow(parent)
-  , ui(std::make_unique<Ui::MainWindow>())
 {
-    ui->setupUi(this);
-    ui->lineEdit->setPlaceholderText("Enter what you want send to server here...");
-    ui->sendButton->setDisabled(true);
-    ui->disconnectButton->setDisabled(true);
-    ui->lineEdit->setDisabled(true);
-    textBrowser = QPointer<QTextBrowser>(ui->textBrowser);
+    tabWidget = new QTabWidget(this);
+    setCentralWidget(tabWidget);
+
+    tab1 = new QWidget();
+    tab2 = new QWidget();
+
+    mainLayout = new QVBoxLayout();
+    connection_layout = new QHBoxLayout();
+    connectButton = new QPushButton("Connect");
+    connectButton->setObjectName("connectButton");
+
+    disconnectButton = new QPushButton("Disconnect");
+    disconnectButton->setObjectName("disconnectButton");
+
+    connectionIndicator = new QRadioButton("Connected");
+    connectionIndicator->setObjectName("connectionIndicator");
+
+    connectionIndicator->setEnabled(false);
+    connectionIndicator->setCheckable(true);
+
+    connection_layout->addWidget(connectButton);
+    connection_layout->addWidget(disconnectButton);
+    connection_layout->addWidget(connectionIndicator);
+
+    user_input_layout = new QHBoxLayout();
+    lineEdit = new QLineEdit("");
+    lineEdit->setObjectName("lineEdit");
+    lineEdit->setPlaceholderText("Enter what you want send to server here...");
+    sendButton = new QPushButton("Send");
+    sendButton->setObjectName("sendButton");
+    sendButton->setDisabled(true);
+
+    user_input_layout->addWidget(lineEdit);
+    user_input_layout->addWidget(sendButton);
+
+    output_layout = new QHBoxLayout();
+    textBrowser = QPointer<QTextBrowser>(new QTextBrowser());
+    textBrowser->setObjectName("textBrowser");
+    output_layout->addWidget(textBrowser);
+
+    mainLayout->addLayout(connection_layout);
+    mainLayout->addLayout(user_input_layout);
+    mainLayout->addLayout(output_layout);
+    tab1->setLayout(mainLayout);
+
+    QVBoxLayout* layout2 = new QVBoxLayout();
+    layout2->addWidget(new QLabel("Nothing special here yet."));
+    tab2->setLayout(layout2);
+
+    tabWidget->addTab(tab1, "Tab 1");
+    tabWidget->addTab(tab2, "Tab 2");
+
+    setWindowTitle("Client");
+    resize(400, 300);
+
+    sendButton->setDisabled(true);
+    disconnectButton->setDisabled(true);
+    lineEdit->setDisabled(true);
+
+    connect(connectButton, &QPushButton::clicked, this, &MainWindow::onConnectClicked);
+    connect(disconnectButton, &QPushButton::clicked, this, &MainWindow::onDisconnectClicked);
+    connect(sendButton, &QPushButton::clicked, this, &MainWindow::onSendClicked);
+
     connection = std::make_shared<Connection>(textBrowser);
-    connect(ui->connectButton, &QPushButton::clicked, this, &MainWindow::onConnectClicked);
-    connect(ui->disconnectButton, &QPushButton::clicked, this, &MainWindow::onDisconnectClicked);
-    connect(ui->sendButton, &QPushButton::clicked, this, &MainWindow::onSendClicked);
 }
 
 void MainWindow::onConnectClicked()
@@ -30,11 +83,11 @@ void MainWindow::onConnectClicked()
         try
         {
             connection->connect();
-            ui->connectionIndicator->setChecked(true);
-            ui->sendButton->setEnabled(true);
-            ui->disconnectButton->setEnabled(true);
-            ui->connectButton->setDisabled(true);
-            ui->lineEdit->setEnabled(true);
+            connectionIndicator->setChecked(true);
+            sendButton->setEnabled(true);
+            disconnectButton->setEnabled(true);
+            connectButton->setDisabled(true);
+            lineEdit->setEnabled(true);
             QMessageBox::information(this, "Info", "Server connected!");
         }
         catch (const boost::system::system_error& e)
@@ -54,16 +107,16 @@ void MainWindow::onDisconnectClicked()
 {
     QMessageBox::information(this, "Info", "Server disconnected!");
     connection->disconnect();
-    ui->connectionIndicator->setChecked(false);
-    ui->sendButton->setDisabled(true);
-    ui->disconnectButton->setDisabled(true);
-    ui->connectButton->setEnabled(true);
-    ui->lineEdit->setDisabled(true);
+    connectionIndicator->setChecked(false);
+    sendButton->setDisabled(true);
+    disconnectButton->setDisabled(true);
+    connectButton->setEnabled(true);
+    lineEdit->setDisabled(true);
 }
 
 void MainWindow::onSendClicked()
 {
-    QString input = ui->lineEdit->text();
+    QString input = lineEdit->text();
     try
     {
         if (!connection->is_connected())
@@ -75,7 +128,7 @@ void MainWindow::onSendClicked()
             if (!input.isEmpty())
             {
                 connection->send(input.toStdString());
-                ui->lineEdit->clear();
+                lineEdit->clear();
             }
         }
     }
--- client/mainwindow.h
+++ client/mainwindow.h
@@ -1,7 +1,12 @@
 #pragma once
 #include "client.hpp"
 #include "connection.hpp"
+#include <QHBoxLayout>
+#include <QLineEdit>
 #include <QMainWindow>
+#include <QPushButton>
+#include <QRadioButton>
+#include <QVBoxLayout>
 #include <boost/asio.hpp>
 #include <memory>
 
@@ -29,7 +34,17 @@ class MainWindow
     void do_write(std::size_t length);
     void display_warning(bool status, std::string s);
     std::shared_ptr<Connection> connection;
-    std::unique_ptr<Ui::MainWindow> ui;
-    std::shared_ptr<boost::asio::io_context> ios;
+    QTabWidget* tabWidget;
+    QPushButton* connectButton;
+    QPushButton* sendButton;
+    QRadioButton* connectionIndicator;
+    QPushButton* disconnectButton;
+    QLineEdit* lineEdit;
+    QVBoxLayout* mainLayout;
+    QHBoxLayout* connection_layout;
+    QHBoxLayout* user_input_layout;
+    QHBoxLayout* output_layout;
+    QWidget* tab1;
+    QWidget* tab2;
     QPointer<QTextBrowser> textBrowser;
 };
