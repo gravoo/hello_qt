a2a99723934cc2e8f36c8dcb70738ff431195004
gravoo <gravoo@yahoo.com>
2025-02-13 13:47:16 +0100

    fix for unresponsive tab

--- client/processes_tab.cpp
+++ client/processes_tab.cpp
@@ -1,9 +1,10 @@
 #include "processes_tab.h"
 #include <QLabel>
 Processes_tab::Processes_tab(QWidget* parent)
+  : QWidget(parent)
 {
-    tab_widget = new QWidget(parent);
-    tab_layout = new QVBoxLayout(parent);
+    tab_widget = new QWidget();
+    tab_layout = new QVBoxLayout();
 
     tab_layout->addWidget(new QLabel("Nothing special here yet."));
     tab_widget->setLayout(tab_layout);
